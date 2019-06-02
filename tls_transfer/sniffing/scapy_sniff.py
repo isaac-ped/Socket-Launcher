c2b621a1ac9aa7025668db95fc9009356240ab99
Isaac Pedisich <iped@seas.upenn.edu>
2019-06-02 16:42:14 +0000

    checks checksums

--- tls_transfer/sniffing/scapy_sniff.py
+++ tls_transfer/sniffing/scapy_sniff.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python
 from scapy.all import *
 import sys
 
-sniff(iface=sys.argv[1], prn=lambda x: x.show())
+def prn_fn(pkt):
+    if TCP not in pkt:
+        return
+
+    pkt.show()
+    chk1 = pkt[TCP].chksum
+    del pkt[TCP].chksum
+
+    pkt2 = Ether(str(pkt))
+    if chk1 != pkt2[TCP].chksum:
+        print("NONMATCH CHKSUM: %x %x" % (chk1, pkt2[TCP].chksum))
+    else:
+        print("MATCH CHKSUM")
+
+
+sniff(iface=sys.argv[1], prn=prn_fn)
