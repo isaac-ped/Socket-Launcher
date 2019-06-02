f2a8953dda860cfd287f6145f966a9630d43f16d
Isaac Pedisich <iped@seas.upenn.edu>
2019-06-02 16:41:13 +0000

    specifying router MAC addresses, disabling offload and timestamps

--- tls_transfer/mininet_topo_iptables.py
+++ tls_transfer/mininet_topo_iptables.py
@@ -30,6 +30,11 @@ class RouterStarTopo(Topo):
     ROUTER = dict(
             ip = '11.0.0.2',
     )
+    ROUTER_MACS = {
+            'r0-eth2': '00:00:00:00:AA:AA',
+            'r0-eth3': '00:00:00:00:BB:BB',
+            'r0-eth4': '00:00:00:00:CC:CC'
+    }
 
     HOSTS = dict(
             h1 = {'ip': '11.0.0.1/16'},
@@ -60,6 +65,8 @@ class RouterStarTopo(Topo):
             )
     )
 
+
+
     def build(self):
         router = self.addNode('r0', cls=LinuxRouter, **self.ROUTER)
 
@@ -70,6 +77,16 @@ class RouterStarTopo(Topo):
         for host in sorted(hosts):
             self.addLink(host, router, **self.LINKS[host])
 
+    def set_macs(self, net):
+        r0 = net.get('r0')
+        for intf, mac in self.ROUTER_MACS.items():
+            r0.intf(intf).setMAC(mac)
+
+    def disable_offload(self, net):
+        for host in self.HOSTS:
+            h = net.get(host)
+            h.cmd("ethtool -K {}-eth0 rx off tx off".format(host))
+            h.cmd('echo "0" > /proc/sys/net/ipv4/tcp_timestamps')
 
 def simpleTest():
     "Create and test a simple network"
@@ -79,7 +96,10 @@ def simpleTest():
     net.start()
     #topo.setIp(net)
 
-    net.get('h2').cmd('bash h2/set_nat.sh')
+    net.get('h2').cmd('bash proxy/set_nat.sh')
+
+    topo.disable_offload(net)
+    topo.set_macs(net)
 
     print "Dumping host connections"
     dumpNodeConnections(net.hosts)
