f41fc983eea4573a781035b9a36be7ca1690f726
Isaac Pedisich <iped@seas.upenn.edu>
2019-06-10 12:21:18 +0000

    adding transferrable echo server

---
+++ tls_transfer/transfer_server.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import socket
+import argparse
+import sys
+import select
+from library.tsock import TransferListenSocket as tsocket
+from library.tsock import set_params
+from threading import Thread
+
+def handle_activity(connection, cli_addr):
+    try:
+        data = connection.recv(1024)
+        print(cli_addr, "Received", data)
+        if data:
+            print(cli_addr, "Echoing", data)
+            connection.sendall(data)
+            if (data.startswith("transfer")):
+                connection.transfer()
+            return True
+        else:
+            print(cli_addr, "Exiting")
+            connection.close()
+            return False
+    except:
+        connection.close()
+        return False
+
+def listen(ip, port):
+    sock = tsocket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((ip, port))
+
+    sock.listen(1)
+
+
+    conns = {}
+    poller = select.poll()
+    poller.register(sock, select.POLLIN)
+    try:
+        while True:
+            activity = poller.poll()
+            for conn in activity:
+                print conn[0], sock
+                if conn[0] == sock.fileno():
+                    connection, cli_addr = sock.accept()
+                    print("Accepted on %d %s" % (connection.fileno(), cli_addr))
+                    poller.register(connection, select.POLLIN)
+                    conns[connection.fileno()] = (connection, cli_addr)
+                else:
+                    if handle_activity(*conns[conn[0]]) == False:
+                        poller.unregister(conn[0])
+    except:
+        sock.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage %s PORT IP IFACE NAME CTL_IP CTL_PORT" % sys.argv[0])
+        exit(-1)
+
+
+    iface = sys.argv[3]
+    name = sys.argv[4]
+    ctl_ip = sys.argv[5]
+    ctl_port = int(sys.argv[6])
+
+    set_params(iface, name, ctl_ip, ctl_port, sys.argv[2])
+    listen(sys.argv[2] if len(sys.argv) > 2 else '', int(sys.argv[1]))
+
